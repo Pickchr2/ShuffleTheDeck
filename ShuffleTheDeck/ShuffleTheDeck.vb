eb2410484ee6fbf40e801b88839a73e13856c294
Chris Pickens <Cpickens1270@gmail.com>
2023-02-25 01:20:30 -0700

    Choose Card

--- ShuffleTheDeck/ShuffleTheDeck.vb
+++ ShuffleTheDeck/ShuffleTheDeck.vb
@@ -12,6 +12,7 @@ Imports System
 Module ShuffleTheDeck
     Dim deckTracker(13, 4) As Boolean
     Dim cardsLeft As Integer
+    Dim displayHand As String
 
     Sub Main(args As String())
         Dim userInput As String
@@ -27,8 +28,58 @@ Module ShuffleTheDeck
                 Case "Q"
                     quitFlag = True
                 Case Else
-
+                    PickACard()
             End Select
         Loop
     End Sub
+
+    Function PickACard() As String
+        Dim cardDrawn As String
+        Dim randomNumber As New Random()
+        Dim randomFace As New Integer
+        Dim randomSuit As New Integer
+
+        randomFace = CInt(randomNumber.Next(13))
+        randomSuit = CInt(randomNumber.Next(4))
+
+        If randomFace = 0 Then
+            cardDrawn = "2"
+        ElseIf randomFace = 1 Then
+            cardDrawn = "3"
+        ElseIf randomFace = 2 Then
+            cardDrawn = "4"
+        ElseIf randomFace = 3 Then
+            cardDrawn = "5"
+        ElseIf randomFace = 4 Then
+            cardDrawn = "6"
+        ElseIf randomFace = 5 Then
+            cardDrawn = "7"
+        ElseIf randomFace = 6 Then
+            cardDrawn = "8"
+        ElseIf randomFace = 7 Then
+            cardDrawn = "9"
+        ElseIf randomFace = 8 Then
+            cardDrawn = "10"
+        ElseIf randomFace = 9 Then
+            cardDrawn = "J"
+        ElseIf randomFace = 10 Then
+            cardDrawn = "Q"
+        ElseIf randomFace = 11 Then
+            cardDrawn = "K"
+        Else
+            cardDrawn = "A"
+        End If
+
+        If randomSuit = 0 Then
+            cardDrawn &= Chr(3) 'Hearts Symbol
+        ElseIf randomSuit = 1 Then
+            cardDrawn &= Chr(4) 'Diamonds Symbol
+        ElseIf randomSuit = 2 Then
+            cardDrawn &= Chr(5) 'Clubs Symbol
+        Else
+            cardDrawn &= Chr(6) 'Spades Symbol
+        End If
+
+        Return cardDrawn
+    End Function
 End Module
