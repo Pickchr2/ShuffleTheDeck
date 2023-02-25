1a74080a7c426dfdeb14a536cacef296367d8002
Chris Pickens <Cpickens1270@gmail.com>
2023-02-25 00:02:21 -0700

    accept user input

--- ShuffleTheDeck/ShuffleTheDeck.vb
+++ ShuffleTheDeck/ShuffleTheDeck.vb
@@ -9,9 +9,26 @@ Option Explicit On
 Option Compare Text
 
 Imports System
-
 Module ShuffleTheDeck
-    Sub Main(args As String())
+    Dim deckTracker(13, 4) As Boolean
+    Dim cardsLeft As Integer
 
+    Sub Main(args As String())
+        Dim userInput As String
+        Dim quitFlag As Boolean = False
+
+        Do Until quitFlag
+            Console.WriteLine("Press " & Chr(34) & "S" & Chr(34) & " to shuffle the deck, Press " & Chr(34) & "Q" & Chr(34) & " to quit, Press any other key to draw a card.")
+            userInput = Console.ReadLine()
+
+            Select Case userInput
+                Case "S"
+
+                Case "Q"
+                    quitFlag = True
+                Case Else
+
+            End Select
+        Loop
     End Sub
 End Module
