be969028af6b2bb66e8bc2567c20c028e4b063ea
Chris Pickens <Cpickens1270@gmail.com>
2023-02-24 23:44:52 -0700

    Add project files.

---
+++ ShuffleTheDeck/ShuffleTheDeck.vb
@@ -0,0 +1,17 @@
+'Christopher Pickens
+'RCET0265
+'Spring 2023
+'Shuffle The Deck
+'https://github.com/Pickchr2/ShuffleTheDeck.git
+
+Option Strict On
+Option Explicit On
+Option Compare Text
+
+Imports System
+
+Module ShuffleTheDeck
+    Sub Main(args As String())
+
+    End Sub
+End Module
